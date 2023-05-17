4984fe3a538e35e28fa12b35e6181d459978dd4b
ManuelB23 <98127595+ManuelB23@users.noreply.github.com>
2023-05-16 21:10:32 -0500

    Update CBox.java

--- domain/CBox.java
+++ domain/CBox.java
@@ -1,10 +1,39 @@
 package domain;
 
 public class CBox {
-    protected int Row,Col;
-    protected Modifiers Modificador;
-    public CBox(int Row,int Col){
+    private int Row;
+    private int Col;
+    private Modifiers Modificador;
+    private int value;
+
+    public CBox(int Row, int Col) {
         this.Row = Row;
         this.Col = Col;
+        this.Modificador = null;
+        this.value = 0;
+    }
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+    
+    public int getRow() {
+        return Row;
+    }
+
+    public int getCol() {
+        return Col;
+    }
+
+    public Modifiers getModificador() {
+        return Modificador;
+    }
+
+    public void setModificador(Modifiers Modificador) {
+        this.Modificador = Modificador;
     }
 }
+
