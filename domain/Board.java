32e8e884e97c79b7532dd8fa7cabeacf9c499001
ManuelB23 <98127595+ManuelB23@users.noreply.github.com>
2023-05-16 21:09:43 -0500

    Update Board.java

--- domain/Board.java
+++ domain/Board.java
@@ -3,14 +3,33 @@ package domain;
 import javax.swing.*;
 
 public class Board {
-    private CBox[][] Casillas;
-    private Token Ficha1,Ficha2;
+    public CBox[][] Casillas;
+    private Token Ficha1, Ficha2;
 
-    public Board(int NumRow,int NumCol){
+    public Board(int NumRow, int NumCol) {
+        Casillas = new CBox[NumRow][NumCol];
         for (int row = 0; row < NumRow; row++) {
             for (int col = 0; col < NumCol; col++) {
-                Casillas = new CBox[NumRow][NumCol];
+                Casillas[row][col] = new CBox(row, col);
             }
+        }
+    }
+    public int getNumRows() {
+        return Casillas.length; }
+    }
+
+    public int getNumCols() {
+        if (Casillas.length > 0) {
+            return Casillas[0].length; }
+        }
+        return 0; }
+    }
+    public int getCellValue(int row, int col) {
+        if (row >= 0 && row < getNumRows() && col >= 0 && col < getNumCols()) {
+            return Casillas[row][col].getValue();
+        } else {
+            
+            return -1; 
         }
     }
 }
