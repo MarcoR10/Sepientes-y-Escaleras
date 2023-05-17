5922ef1c844734e500d817e22d628f38fb1c789b
ManuelB23 <98127595+ManuelB23@users.noreply.github.com>
2023-05-16 21:10:53 -0500

    Update Player.java

--- domain/Player.java
+++ domain/Player.java
@@ -6,7 +6,7 @@ public class Player {
     private String Name;
     private Color color;
 
-    public Player(String Name,Color color){
+    public Player(String Name, Color color){
         this.Name = Name;
         this.color = color;
     }
